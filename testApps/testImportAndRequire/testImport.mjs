0153fb6756aedf4eec16fa215084d6bb955a34b4
nir maoz <nir.maoz@cloudinary.com>
2022-08-04 07:56:58 +0300

    tests: remove unneeded logging

--- testApps/testImportAndRequire/testImport.mjs
+++ testApps/testImportAndRequire/testImport.mjs
@@ -11,4 +11,3 @@ const myImage = cld.image('sample');
 myImage.resize(Resize.scale().width(100).height(100));
 
 const myURL = myImage.toURL();
-console.log(myURL);
--- testApps/testImportAndRequire/testRequire.js
+++ testApps/testImportAndRequire/testRequire.js
@@ -13,4 +13,3 @@ const myImage = cld.image('sample');
 myImage.resize(Resize.scale().width(100).height(100));
 
 const myURL = myImage.toURL();
-console.log(myURL);
