e36d3d6855a6a7c233e91b19c0a7e9992d4a9d45
Nir Maoz <47939921+maoznir@users.noreply.github.com>
2022-08-07 13:44:40 +0300

    docs: remove examples containing secure param usage (#4)

--- testApps/testImportAndRequire/testImport.mjs
+++ testApps/testImportAndRequire/testImport.mjs
@@ -4,14 +4,10 @@ import { Resize } from '@cloudinary/url-builder/actions/resize';
 const cld = new Cloudinary({
   cloud: {
     cloudName: 'test',
-  },
-  url: {
-    secure: true,
-  },
+  }
 });
 
 const myImage = cld.image('sample');
 myImage.resize(Resize.scale().width(100).height(100));
 
 const myURL = myImage.toURL();
-console.log(myURL);
--- testApps/testImportAndRequire/testRequire.js
+++ testApps/testImportAndRequire/testRequire.js
@@ -6,14 +6,10 @@ const { Resize } = require('@cloudinary/url-builder/actions/resize');
 const cld = new Cloudinary({
   cloud: {
     cloudName: 'test',
-  },
-  url: {
-    secure: true,
-  },
+  }
 });
 
 const myImage = cld.image('sample');
 myImage.resize(Resize.scale().width(100).height(100));
 
 const myURL = myImage.toURL();
-console.log(myURL);
